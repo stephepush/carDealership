453234b8c0a0b3d2f06b9b40f46f62e5cbcfc86d
Stephen <7speter@gmail.com>
2021-09-06 01:02:23 -0400

    Adds img & alt tag to ejs template

--- controllers/admin.js
+++ controllers/admin.js
@@ -11,6 +11,22 @@ const Car = require('../models/car');
    
 } */
 
+/* exports.getVehicles = (req, res, next) => {
+    Car.fetchAll()
+        .then((rows) => {
+            console.log(rows[0]);
+            
+            res.render('admin/vehicles', {
+                cars: rows[0],
+                pageTitle: 'Dealer Admin Page',
+                path: '/admin/vehicles',
+                
+            }); 
+            
+        })
+        .catch(err => console.log(err));
+} */
+
 exports.getVehicles = (req, res, next) => {
     Car.fetchAll()
         .then((rows) => {
