28f4b4ec4921194729b19ac2ed9f40cc33d4bc0b
Stephen <7speter@gmail.com>
2021-09-07 13:12:19 -0400

    Restores previous route handling in app.js

--- app.js
+++ app.js
@@ -2,21 +2,21 @@ const path = require('path');
 //const bodyParser = require('body-parser');
 
 const express = require('express');
-const mountRoutes = require('./routes')
+//const mountRoutes = require('./routes')
 
 const app = express();
-mountRoutes(app)
+//mountRoutes(app)
 
 app.use(express.urlencoded({ extended: true }));
 
-//const adminRoutes = require('./routes/admin');
-//const lotRoutes = require('./routes/lot');
+const adminRoutes = require('./routes/admin');
+const lotRoutes = require('./routes/lot');
 
 app.set('view engine', 'ejs');
 app.set('views', 'views');
 
-//app.use('/admin', adminRoutes);
-//app.use(lotRoutes);
+app.use('/admin', adminRoutes);
+app.use(lotRoutes);
 
 app.use((req, res, next) => {
     res.status(404).render(
