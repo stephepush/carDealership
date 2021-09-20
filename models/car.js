13da14cd2a9463719406574caa8d210e77c299b7
Stephen <7speter@gmail.com>
2021-09-20 15:27:03 -0400

    Tidies up car model and database file

--- models/car.js
+++ models/car.js
@@ -3,19 +3,6 @@ const  db = require('./database');
 //import { conn } from './database';
 
 
-//connection.end(); */
-
-/* conn
-  .query("SELECT NOW()")
-  .then(rows => {
-	console.log(rows); //[ { 'NOW()': 2018-07-02T17:06:38.000Z }, meta: [ ... ] ]
-  })
-  .catch(err => {
-	//handle error
-  });
- */
-//const pool = new Pool;
-//change name to Vehicle in a later commit
 module.exports = class Car {
     constructor( 
         id, model_year, make, model, miles,
@@ -36,25 +23,6 @@ module.exports = class Car {
         }
 
 
-        static create(id, model_year, make, model, miles,
-            color, transmission, layout, engine_config, car_photo_url, car_price) {
-                db.tx(async t => {
-                    await t.none('INSERT INTO cars(model_year, make, model, miles, color, transmission, layout, engine_config) VALUES($1, $2, $3, $4, $5, $6, $7, $8)', [/*input from forms*/]);
-                    await t.none('INSERT INTO car_photos(car_photo_url) VALUES($1)', [/*Input from forms */]);
-                })
-                .then(() => {
-                    console.log("success!")
-                })
-                .catch(error => {
-                    console.log('ERROR:', error);
-                })
-        }
-
-/*         
-        static fetchAll () {
-            return db.query('SELECT * FROM cars ')
-        } */
-
         static fetchAll () {
             var options = {
                 sql: 
@@ -68,24 +36,6 @@ module.exports = class Car {
             return db.execute('SELECT * FROM products WHERE products.id = ?', [id]);
         }
 
-/*         save() {
-            console.log("hi")
-            
-            return db.execute(
-                'INSERT INTO cars (model_year, make, model, color, miles, transmission, layout, engine_type) VALUES (?, ?, ?, ?, ?, ?, ?, ?)',
-                [
-                    this.model_year, 
-                    this.make, 
-                    this.model, 
-                    this.color,
-                    this.miles,
-                    this.transmission,
-                    this.layout,
-                    this.engine_type
-                ]
-            )
-        } */
-
         save() {
             let pk;
             
@@ -97,10 +47,7 @@ module.exports = class Car {
                 .then(() => {
                     const car_insert = db.query('INSERT INTO cars (model_year, make, model, color, miles, transmission, layout, engine_type) VALUES (?, ?, ?, ?, ?, ?, ?, ?)',
                     [this.model_year, this.make, this.model, this.color,this.miles, this.transmission, this.layout, this.engine_type]);
-                    return car_insert
-                    ;
-                    
-                    
+                    return car_insert;
                 })
                  .then((car_insert) => {
                     console.log(Object.entries(car_insert));
@@ -127,63 +74,4 @@ module.exports = class Car {
         }
 
         static deleteById(id) {}
-
-        
-        
-/*         static fetchAll = async () => {
-            //const pool = new Pool;
-            
-            console.log('starting async query')
-            const result = await selectQuery('SELECT * FROM cars')
-            console.log(result)
-            console.log('async query finished')
-            return result
-
-
-            console.log('calling end')
-            //await pool.end()
-            console.log('pool has drained')
-          } */
-        
-
-        /* static async fetchAll () {
-        
-            console.log('Displaying')
-            try {
-                let results= await paramlessQuery('SELECT * FROM cars');
-                console.log(results)
-                console.log('end');
-                results.status(200).json(results)
-                //return results
-            } catch (err) {
-                console.log(err)
-                return err
-            }
-            
-            //console.log(results.rows)
-            //return results
-            
-        } */
-
-/*         static fetchAll = async()=> {
-            return new Promise(async function(resolve, reject){
-                try {
-                    let results = await query('SELECT * FROM cars');
-                    console.log(results)
-                    return resolve(results);
-                } catch (err) {
-                    console.log(err)
-                    return reject(err);
-                }
-
-                console.log(results.rows)
-               //return results
-            })
-            
-            
-        } */
-
-/*      static findById(id) {
-            //return db.execute('SELECT * FROM products WHERE products.id = ?', [id]);
-        } */
 }
--- models/database.js
+++ models/database.js
@@ -8,41 +8,6 @@ const pool = mysql.createPool({
   database: 'express_autos',
   //multipleStatements: true
 })
-/* 
-pool.connect(function(error){
-  if(!!error){
-    console.log(error);
-  }else{
-    console.log('Connected!:)');
-  }
-}); */
-/* .then(conn => {
-  conn.query("select 1", [2])
-    .then(rows => {
-      console.log(rows); // [{ "1": 1 }]
-      conn.end();
-    })
-    .catch(err => { 
-      //handle query error
-      console.log('hello')
-    });
-})
-.catch(err => {
-  //handle connection error
-  console.log('there seems to be an error: ' + err.stack)
-}); 
- */
 
-/* let config = {
-  host     : '127.0.0.1',
-  port     : 3306,
-  user     : 'admin',
-  password : 'hello',
-  database : 'express-autos'
-}; */
-
-/* let connection = mysql.createConnection(config)*/
-
-//pool.connect()
 module.exports = pool.promise();
-  //connection : mariadb.createConnection(config) 
+
