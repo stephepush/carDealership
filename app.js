d557040b8250276102537f54b4dc6369279a531d
Stephen <7speter@gmail.com>
2021-09-27 14:40:42 -0400

    Revises bootstrap classes on edit-vehicle template

--- app.js
+++ app.js
@@ -35,7 +35,7 @@ app.use((req, res, next) => {
             './404', 
             { 
                 pageTitle: '404 Page',
-                editing: false
+                pageName: 'not_found',
             }
         )
 })
