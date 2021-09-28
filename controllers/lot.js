cb01583240762ca760b303180927928867865356
Stephen <7speter@gmail.com>
2021-09-28 19:37:25 -0400

    Begins addressing big picture controller/router issues and creates auth.js route

--- controllers/lot.js
+++ controllers/lot.js
@@ -5,14 +5,14 @@ exports.getVehicles = (req, res, next) => {
     Car.fetchAll()
         .then((rows) => {
             console.log(rows[0]);
-            
+
             res.render('admin/vehicles', {
                 cars: rows[0],
                 pageTitle: 'Dealer Admin Page',
                 path: '/admin/vehicles',
-                
-            }); 
-            
+
+            });
+
         })
         .catch(err => console.log(err));
 }
