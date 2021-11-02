618160ea2cf6570ce8a631b86cbeb6f04b803a18
Stephen <7speter@gmail.com>
2021-11-01 22:54:55 -0400

    Adds first attempt at postAddUser in user controller

---
+++ controllers/user.js
@@ -0,0 +1,23 @@
+const User = require('../models/user');
+
+exports.postAddUser = (req, res, next) => {
+    const email = req.body.email;
+    const username = req.body.username;
+    const hash;
+    const salt;
+    const person_img = req.body.person_img;
+    const dob = req.body.dob;
+    const gender = req.body.gender;
+    const newsletter = req.body.gender;
+
+    const user = new User(
+        null, email, username, hash, salt, person_img, dob, gender, newsletter
+    );
+    console.log(user);
+    user
+        .save()
+        .then(() => {
+            res.redirect('/');
+        })
+        .catch(err => console.log(err))
+}
