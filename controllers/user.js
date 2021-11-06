f474cd2b61df0920c6308010beb30c0fd7b7e55e
Stephen <7speter@gmail.com>
2021-11-06 00:25:34 -0400

    Adds hashPassword() to /lib/passwordUtil.js, requires it in user controller

--- controllers/user.js
+++ controllers/user.js
@@ -1,17 +1,19 @@
 const User = require('../models/user');
+const { hashPassword } = require('../lib/passwordUtils/')
 
 exports.postAddUser = (req, res, next) => {
     const email = req.body.email;
     const username = req.body.username;
-    //const hash;
-    //const salt;
+    const hash = hashPassword(req.body.password);
+    const salt;
+    const isAdmin = 0;
     const person_img = req.body.person_img;
     const dob = req.body.dob;
     const gender = req.body.gender;
     const newsletter = req.body.gender;
 
     const user = new User(
-        null, email, username, hash, salt, person_img, dob, gender, newsletter
+        null, email, username, hash, salt, person_img, dob, gender, newsletter, isAdmin
     );
     console.log(user);
     user
