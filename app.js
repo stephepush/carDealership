72bd849be748eedffc6718a59e3aa12e36e485c2
Stephen <7speter@gmail.com>
2021-11-17 14:24:19 -0500

    Adds passport.initialize & passport.session to app.js

--- app.js
+++ app.js
@@ -38,6 +38,9 @@ app.use(session({
         maxAge: 1000 * 60 * 60 * 24
     }
 }))
+app.use(passport.initialize());
+app.use(passport.session());
+
 
 app.use(express.urlencoded({ extended: true }));
 
