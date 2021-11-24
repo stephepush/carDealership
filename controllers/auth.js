594582afaa6015305e561a273353e59ee709924f
Stephen <7speter@gmail.com>
2021-11-24 12:06:10 -0500

    Gets initial basic passport-local workflow working

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,16 +1,26 @@
+const passport = require('passport');
+const { validatesPassword } = ('../lib/passwordUtils')
+
+
+
+
+exports.postLogin =
+    //passport takes the reigns for this route
+    passport.authenticate('local',
+
+        {
+            failureRedirect: "/login-failure",
+            successRedirect: "/login-success"
+        }
+    )
+
+
 exports.getLogin = (req, res, next) => {
     //console.log(req.get('Cookie').split(';')[1]);
+
     res.render('auth/login', {
         path: '/login',
         pageTitle: 'Login to Express Autos',
-        isAuthenticated: req.isLoggedIn
+        //isAuthenticated: req.isLoggedIn
     })
-}
-
-exports.postLogin = (req, res, next) => {
-
-    res.setHeader('Set-Cookie', 'loggedIn = true');
-    console.log("Here we are : root");
-    res.redirect('/');
-
 }
