705146853f7a68483ec57749b8b253675a0eeda7
Stephen <7speter@gmail.com>
2021-11-24 12:03:14 -0500

    Changes order of auth postLogin route

--- routes/auth.js
+++ routes/auth.js
@@ -6,8 +6,16 @@ const authController = require('../controllers/auth');
 
 const router = new Router()
 
+router.post('/login', authController.postLogin);
+
 router.get('/login', authController.getLogin);
 
-router.post('/login', authController.postLogin)
+router.get('/login-success', (req, res, next) => {
+    res.send('<p>You successfully logged in. --> <a href="/protected-route">Go to protected route</a></p>');
+});
+
+router.get('/login-failure', (req, res, next) => {
+    res.send('You entered the wrong password.');
+});
 
 module.exports = router;
