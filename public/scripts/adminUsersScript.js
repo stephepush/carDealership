1f11bc3f131dfa7c6815ff5a6e89c7bcb7fd1867
Stephen Peters <7speter@gmail.com>
2022-08-10 01:16:12 -0400

    Adds 1st attempt at fetch in public admin/user js file

--- public/scripts/adminUsersScript.js
+++ public/scripts/adminUsersScript.js
@@ -13,5 +13,24 @@ let userIdNumber = document.getElementById('hiddenUserId')
 }) */
 userTypeButton.addEventListener('click', (event)=>{
     console.log(userTypeSelect.value);
-    console.log(userIdNumber.value)
+    console.log(userIdNumber.value);
+    
+    fetch("/admin/user/userType", {
+        method: "post",
+        headers: {
+            'Accept': 'application/json',
+            'Content-Type' : 'application/json'
+        },
+        body: JSON.stringify({
+           userId: userIdNumber.value,
+           userType: userTypeSelect.value
+        })
+        
+    })
+    .then(result => {
+            console.log(result)
+        })
+        /*.catch(err => {
+            console.log(err)
+        })*/
 })
