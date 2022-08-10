7608ded23d85c202ffb0b623d7d0ba5bd078f622
Stephen Peters <7speter@gmail.com>
2022-08-09 23:29:41 -0400

    Adds necessary variable and tests if its working right

--- public/scripts/adminUsersScript.js
+++ public/scripts/adminUsersScript.js
@@ -6,10 +6,12 @@ const userTypeButton = document.querySelector('#user-type-submit')
 let userTypeSelect = document.getElementById('user-type-select')
 let selectElementValue = userTypeSelect.options[userTypeSelect.selectedIndex].value;
 let selectElementText = userTypeSelect.options[userTypeSelect.selectedIndex].value;
+let userIdNumber = document.getElementById('hiddenUserId')
 
 /* userTypeButton.addEventListener('click', (event)=>{
     console.log(userTypeSelect.options[userTypeSelect.selectedIndex].value)
 }) */
 userTypeButton.addEventListener('click', (event)=>{
     console.log(userTypeSelect.value);
+    console.log(userIdNumber.value)
 })
