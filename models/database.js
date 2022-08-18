cce9b110322ca643f2e417c0cf2ef4c0550b34c0
Stephen Peters <7speter@gmail.com>
2022-08-17 23:06:59 -0400

    temporary static changes to db that needs to be env'd

--- models/database.js
+++ models/database.js
@@ -4,7 +4,7 @@ const pool = mysql.createPool({
     host: 'localhost',
     user: 'dbeaver_user',
     password: 'password',
-    database: 'express_autos_demo',
+    database: 'express_autos',
     //multipleStatements: true
 })
 
